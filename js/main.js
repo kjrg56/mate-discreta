2c64917f464187cf1bf88ecfc3a32e5bbd444a16
Kevin Raymundo <kjrg56@gmail.com>
2018-10-15 23:47:11 -0600

    se comienza grafos

--- js/main.js
+++ js/main.js
@@ -1,41 +1,50 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
+    $('select').formSelect();
     showView('home.html');
 });
 
 function showView(viewUrl) {    
-    $.get(viewUrl, function( data ) {
+    $.get(viewUrl, function(data) {
         $(".view").html(data);
     });
 }
 
 // GRAFOS
 
-var countId = 0;
+var labelCount = 65;
 var vertices = [];
-
-$( "#grafoCanvas" ).click(function(e) {
-    drawVertice(e);
-});
 
 function drawVertice(e) {
     var canvas = document.getElementById("grafoCanvas");
-    var ctx = canvas.getContext("2d");
-    
     var coords = getCoordinates(e, canvas);
+    var ctx = canvas.getContext("2d");
 
     ctx.beginPath();
-    ctx.arc(coords.x, coords.y, 5, 0, 2*Math.PI);
+    ctx.arc(coords.x, coords.y, 10, 0, 2*Math.PI);
     ctx.stroke();
+    ctx.font = "15px Arial";
+    ctx.fillText(getLabel(labelCount), coords.x-5, coords.y+5);
 
     var vertice = {
-        id: countId++,
+        name: getLabel(labelCount),
         x: coords.x,
         y: coords.y
     }
 
+    var deSelect = document.getElementById('deVertice');
+    var aSelect = document.getElementById('aVertice');
+    deSelect.options.add(new Option(vertice.name, vertice.name));
+    aSelect.options.add(new Option(vertice.name, vertice.name));
+    $('select').formSelect();
+
     vertices.push(vertice);
-    console.log(vertices);
+    labelCount++;
+    console.log(vertice);
+}
+
+function getLabel(unicode) {
+    return String.fromCharCode(unicode);
 }
 
 function getCoordinates(evt, canvas) {
@@ -50,31 +59,30 @@ function getCoordinates(evt, canvas) {
       }
 }
 
-function drawGrafo() {
+function drawArista() {
+    var deLabel = $('#deVertice').val();
+    var aLabel = $('#aVertice').val();
+
     var canvas = document.getElementById("grafoCanvas");
     var context = canvas.getContext('2d');
 
-    var v1 = vertices[0];
-    var v2 = vertices[1];
-    var v3 = vertices[2];
-    var v4 = vertices[3];
+    var deVertice;
+    var aVertice;
 
-    context.moveTo(v1.x, v1.y);
-    context.lineTo(v2.x, v2.y);
-    context.stroke();
-
-    context.moveTo(v2.x, v2.y);
-    context.lineTo(v3.x, v3.y);
-    context.stroke();
-
-    context.moveTo(v3.x, v3.y);
-    context.lineTo(v4.x, v4.y);
-    context.stroke();
-
-    context.moveTo(v4.x, v4.y);
-    context.lineTo(v1.x, v1.y);
-    context.stroke();
-
+    if (vertices.length > 0) {
+        for (i = 0; i < vertices.length; i++) {
+            if (vertices[i].name === deLabel) {
+                deVertice = vertices[i];
+            } else if (vertices[i].name === aLabel) {
+                aVertice = vertices[i];
+            } 
+        }
+        context.moveTo(deVertice.x, deVertice.y);
+        context.lineTo(aVertice.x, aVertice.y);
+        context.stroke();    
+    } else {
+        swal('No existen vértices', 'Por favor, situé vertices en el área de trabajo.', 'warning');
+    }
 }
 
 function clearCanvas() {
@@ -82,6 +90,20 @@ function clearCanvas() {
     var context = canvas.getContext('2d');
     context.clearRect(0, 0, canvas.width, canvas.height);
     vertices = [];
+    countId = 0;
+    labelCount = 65;
+
+    var deSelect = document.getElementById('deVertice');
+    var aSelect = document.getElementById('aVertice');
+
+    while (deSelect.hasChildNodes() && aSelect.hasChildNodes()) {   
+        deSelect.removeChild(deSelect.firstChild);
+        aSelect.removeChild(aSelect.firstChild);
+    }
+
+    deSelect.options.add(new Option('DE: ', '', false, true));
+    aSelect.options.add(new Option('A:', '', false, true));
+    $('select').formSelect();
 }
 
 // MCD
